d9ea2bde50487cc88d7d34b58550900416a1ed1d
KyuminLee <kyumin1404@gmail.com>
2024-05-15 20:18:17 +0900

    initial commit

---
+++ goodmenu-mapcbnu/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NaverMapComponent from './NaverMapComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Nearby Restaurants</h1>
+      <NaverMapComponent />
+    </div>
+  );
+}
+
+export default App;
