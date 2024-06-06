13d92166087f6a0af006f434ada294711e339312
KyuminLee <kyumin1404@gmail.com>
2024-06-06 22:26:01 +0900

    test 6

--- script.js
+++ script.js
@@ -15,7 +15,14 @@ var markers = [
   { position: new naver.maps.LatLng(36.626117, 127.452091), category: '카페', name: '보통의 국수집' },
   { position: new naver.maps.LatLng(36.625669, 127.463756), category: '펜션', name: '구룡산 한식' },
   { position: new naver.maps.LatLng(36.624039, 127.450508), category: '주차장', name: '현고 들깨칼국수' },
-  { position: new naver.maps.LatLng(36.624639, 127.467022), category: '쿠폰', name: '사나이 짬뽕' }
+  { position: new naver.maps.LatLng(36.624639, 127.467022), category: '쿠폰', name: '사나이 짬뽕' },
+  { position: new naver.maps.LatLng(36.627527, 127.452819), category: '편의점', name: '양성재 CU 편의점' },
+  { position: new naver.maps.LatLng(36.627766, 127.458847), category: '편의점', name: '제 1학생회관 이마트24 편의점' },
+  { position: new naver.maps.LatLng(36.630457, 127.456008), category: '편의점', name: '대학본부 이마트24 편의점' },
+  { position: new naver.maps.LatLng(36.623980, 127.459104), category: '편의점', name: '양진재 CU 편의점' },
+  { position: new naver.maps.LatLng(36.625097, 127.457317), category: '편의점', name: '학연산 세븐일레븐 편의점' },
+  { position: new naver.maps.LatLng(36.628456, 127.457540), category: '도서관', name: '중앙도서관' },
+  { position: new naver.maps.LatLng(36.628140, 127.459553), category: '서점', name: '서점'}
 ];
 
 var naverMarkers = [];
