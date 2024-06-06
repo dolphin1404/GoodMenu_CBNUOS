99525a6c3e217a59bc396da8358c15df87934b31
KyuminLee <kyumin1404@gmail.com>
2024-06-06 19:25:24 +0900

    test 3

--- goodmenu-map/src/index.js
+++ goodmenu-map/src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Add this line
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    );
-} else {
-    console.error('Failed to find the root element');
-}
-
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
